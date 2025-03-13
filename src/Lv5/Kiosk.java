e61893f3e1e485817d59619174dcb71a50a39253
giwon-wg <giwon8766@naver.com>
2025-03-13 20:22:12 +0900

    10. 로그인 및 포인트 저장 관리

--- src/Lv5/Kiosk.java
+++ src/Lv5/Kiosk.java
@@ -22,9 +22,12 @@ public class Kiosk {
         Scanner sc = new Scanner(System.in);
         int num = -1;
 
+        System.out.print("사용자 이름: ");
+        String userId = sc.nextLine();
+
         //입력값이 0일경우 프로그램 종료
         while (num != 0) {
-            System.out.println("\n====================");
+            System.out.println("\n=====================");
             System.out.println("[SHAKESHACK MENU]");
 
             //반복문을 활용해 List 안에 있는 MenuItem을 하나씩 출력
@@ -32,7 +35,7 @@ public class Kiosk {
                 System.out.println(TextColor.R + (i + 1) + ". " + TextColor.E + menus.get(i).getCategory());
             }
             System.out.println(TextColor.R + "0. " + TextColor.E + "뒤로가기");
-            System.out.println("====================");
+            System.out.println("=====================");
 
 //            장바구니 구현
             if(!cart.isEmpty()){
@@ -52,7 +55,7 @@ public class Kiosk {
                     display(select, sc);
                 }else {
                     try {
-                        cart(numinput, sc);
+                        cart(numinput, sc, userId);
                     } catch (IllegalStateException e) {
                         System.out.println(e.getMessage());
                     }
@@ -64,7 +67,7 @@ public class Kiosk {
     }
 
     private void display(Menu menu, Scanner sc) {
-        System.out.println("\n====================");
+        System.out.println("\n=====================");
         System.out.println("[" + menu.getCategory() + " MENU]");
 
 
@@ -74,7 +77,7 @@ public class Kiosk {
             items.get(i).getprint();
         }
         System.out.println(TextColor.R + "0. " + TextColor.E + "Move to Main");
-        System.out.println("====================");
+        System.out.println("=====================");
 
         if (sc.hasNextInt()) {
             int numinputs = sc.nextInt();
@@ -86,10 +89,10 @@ public class Kiosk {
             } else if (numinputs >= 1 && numinputs <= items.size()) {
                 //선택한 메뉴 설명
                 MenuItem select = items.get(numinputs - 1);
-                System.out.println("\n====================");
+                System.out.println("\n=====================");
                 System.out.println("메뉴 설명");
                 select.choose();
-                System.out.println("====================");
+                System.out.println("=====================");
                 System.out.println(TextColor.R + "1. " + TextColor.E + "장바구니에 담기");
                 System.out.println(TextColor.R + "0. " + TextColor.E + "Move to Main");
 
@@ -147,10 +150,10 @@ public class Kiosk {
         System.out.println(TextColor.R + (menus.size() + 1) + ". " + TextColor.E + "상품 구매하기");
         System.out.println(TextColor.R + (menus.size() + 2) + ". " + TextColor.E + "전체 비우기");
         System.out.println(TextColor.R + (menus.size() + 3) + ". " + TextColor.E + "선택 비우기");
-        System.out.println("====================");
+        System.out.println("=====================");
     }
 
-    public void order(Scanner sc) {
+    public void order(Scanner sc, String userId) {
         double totalprice = 0;
         int totalpriceInt;
         if (cart.isEmpty()) {
@@ -167,17 +170,17 @@ public class Kiosk {
             System.out.println("\n총 가격: W " + totalpriceInt);
         }
         System.out.println("총 결제금액은 " + totalpriceInt + "원입니다.");
-        System.out.println("결제 하시겠습니까? Yes : 1 / No : 2");
-        System.out.println("====================");
+        System.out.println("결제 하시겠습니까?\nYes : 1, No : 2");
+        System.out.println("=====================");
         String yorn = sc.nextLine();
         if (yorn.equals("1")) {
-            System.out.println("====================");
+            System.out.println("=====================");
             System.out.println("할인혜택을 선택해주세요");
             System.out.println("1. 국가유공자 (10% 할인)");
             System.out.println("2. 군인 (5% 할인)");
             System.out.println("3. 학생 (3% 할인)");
             System.out.println("4. 일반");
-            System.out.println("====================");
+            System.out.println("=====================");
             Discount discounttpye = Discount.NON;
             String userType = "";
             if(sc.hasNextInt()){
@@ -207,12 +210,7 @@ public class Kiosk {
             }
             int disconutPrice = (int) (totalpriceInt * discounttpye.getDiscounType());
             int totalPriceDisconut = totalpriceInt - disconutPrice;
-            System.out.println("====================");
-            System.out.println(userType + " 할인금액은 " + disconutPrice + "원입니다.");
-            System.out.println("최종 결제금액은 " + totalPriceDisconut + "원입니다.");
-            System.out.println("이용해주셔서 감사합니다.");
-            System.out.println("====================");
-            cart.clear();
+            pay(sc, userType, disconutPrice, totalPriceDisconut, userId);
         } else if (yorn.equals("2")) {
             System.out.println("주문 결제가 취소되었습니다.");
         } else {
@@ -250,15 +248,15 @@ public class Kiosk {
         }
     }
 
-    public void cart(int numinput, Scanner sc){
+    public void cart(int numinput, Scanner sc, String userId){
         if(cart.isEmpty() && (numinput == menus.size() + 1 || numinput == menus.size() + 2 || numinput == menus.size() + 3)){
             throw new IllegalStateException("**올바른 숫자를 입력해주세요");
         }
 
         if (numinput == menus.size() + 1) {//메뉴 추가 대비 하드코딩X
             System.out.println("구매창으로 이동합니다.\n");
-            System.out.println("====================");
-            order(sc);
+            System.out.println("=====================");
+            order(sc, userId);
         } else if (numinput == menus.size() + 2){
             cart.clear();
             System.out.println("장바구니를 비웠습니다.");
@@ -266,7 +264,111 @@ public class Kiosk {
             System.out.println("삭제 하실 제품을 선택해주세요");
             removeCart(sc);
         } else {
-            System.out.println("입력값을 확인해 주세요");
+            System.out.println("**입력값을 확인해 주세요");
         }
+    }
+
+    public void pay(Scanner sc, String userType, int disconutPrice, int totalPriceDisconut, String userId){
+        System.out.println("=====================");
+        System.out.println(userType + " 할인금액은 " + disconutPrice + "원입니다.");
+        System.out.println("최종 결제금액은 " + totalPriceDisconut + "원입니다.\n");
+        System.out.println("=====================");
+        System.out.println("[결제 방법을 선택해주세요]");
+        System.out.println("1. 신용카드(할부 결제는 지원하지 않습니다.)");
+        System.out.println("2. 체크카드");
+        System.out.println("3. 현금");
+        System.out.println("=====================");
+
+        if (sc.hasNextInt()) {
+            int input = sc.nextInt();
+            sc.nextLine();
+
+            PayType payType = null;
+            switch (input) {
+                case 1:
+                    payType = PayType.CREDITCASH;
+                    break;
+                case 2:
+                    payType = PayType.ATMCARD;
+                    break;
+                case 3:
+                    payType = PayType.CASH;
+                    break;
+                default:
+                    System.out.println("**입력값을 확인해 주세요");
+                    return;
+            }
+            payment(sc, payType, totalPriceDisconut, userId);
+        }
+    }
+
+    public void payment(Scanner sc, PayType payType, int totalPrice, String userId){
+        System.out.println("=====================");
+        System.out.println(payType.getPaytypes() + "로 결제를 진행합니다.");
+
+        int[] userData = UserData.loadUserData(userId);
+        int userCash = userData[0];
+        int userPoint = userData[1];
+        int pointDiscountPrice;
+        int discountPoint;
+        System.out.println("적립 포인트를 사용하시겠습니까?\nYES: 1, NO: 2");
+
+
+        if(sc.hasNextInt()){
+            int input = sc.nextInt();
+            sc.nextLine();
+            if (input == 1){
+                System.out.println("몇 포인트를 사용하시겠습니까?\n보유포인트: "+ userPoint);
+                discountPoint = sc.nextInt();
+                if(discountPoint < 1000){
+                    System.out.println("1000점 이상만 사용 가능합니다.");
+                    return;
+                }
+                if(discountPoint > totalPrice){
+                    System.out.println("주문금액 이상으로 사용하실 수 없습니다.");
+                    return;
+                }
+                if(discountPoint > userPoint){
+                    System.out.println("보유 포인트 이상으로 사용하실 수 없습니다.");
+                    return;
+                }
+                pointDiscountPrice = totalPrice - discountPoint;
+                int point = (int) (pointDiscountPrice * 0.01);
+                userPoint -= discountPoint;
+                userPoint += point;
+                System.out.println("=======[결제창]=======");
+                System.out.println("      금액: " + totalPrice);
+                System.out.println("사용 포인트: " + discountPoint);
+                System.out.println("최종 결제액: " + pointDiscountPrice);
+                System.out.println("---------------------");
+                System.out.println("적립 포인트: " + point);
+                System.out.println("현재 포인트: " + userPoint);
+                System.out.println("=====================");
+                System.out.println("결제가 완료되었습니다.");
+            }else if(input == 2){
+                int point = (int) (totalPrice * 0.01);
+                userPoint += point;
+                System.out.println("=======[결제창]=======");
+                System.out.println("최종 결제액: " + totalPrice);
+                System.out.println("---------------------");
+                System.out.println("적립 포인트: " + point);
+                System.out.println("현재 포인트: " + userPoint);
+                System.out.println("=====================");
+                System.out.println("결제가 완료되었습니다.");
+
+            }else {
+                System.out.println("**입력값을 확인해 주세요");
+                return;
+            }
+        }else{
+            System.out.println("**숫자로 입력해주세요.");
+            sc.nextLine();
+            return;
+        }
+        if(payType == PayType.CASH && userCash < totalPrice){
+            System.out.println("잔액이 부족합니다.");
+        }
+        UserData.saveUserData(userId, userCash, userPoint);
+        cart.clear();
     }
 }
--- src/Lv5/UserData.java
+++ src/Lv5/UserData.java
@@ -1,4 +1,44 @@
 package Lv5;
 
+import java.io.*;
+import java.util.Properties;
+
 public class UserData {
+    private static final String UserName = "Userdata_";
+
+
+    public static int[] loadUserData(String userId){
+        Properties users = new Properties();
+        String fileName = UserName + userId + ".properties";
+        File file = new File(UserName);
+
+
+        //파일이 없다면? -> 신규 이용자로 등록
+        if(!file.exists()){
+            saveUserData(userId, 0, 0);
+            return new int[]{0, 1000};
+        }
+
+        try(FileInputStream fileinput = new FileInputStream(file)){
+            users.load(fileinput);
+            int cash = Integer.parseInt(users.getProperty("cash", "0"));
+            int point = Integer.parseInt(users.getProperty("point", "0"));
+            return new int[]{0, 0};
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return new int[]{0, 0};
+    }
+
+    public static void saveUserData(String userID, int cash, int point){
+        Properties user = new Properties();
+        user.setProperty("cash", String.valueOf(cash));
+        user.setProperty("point", String.valueOf(point));
+
+        try(FileOutputStream fos = new FileOutputStream(UserName + userID + ".properties")){
+            user.store(fos, "user data (cash, point");
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
